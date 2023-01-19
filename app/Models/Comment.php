81374af1e13ef9de04d7c2f05011299f140f7512
nagimasa <nagimasa.0114@gmail.com>
2023-01-19 12:22:55 +0900

    コメントのモデルとマイグレーションとコントローラを作成

---
+++ app/Models/Comment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+use App\Models\Service;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'comment',
+    ];
+
+
+
+    public function services()
+    {
+        return $this->hasOne(Service::class);
+    }
+}
