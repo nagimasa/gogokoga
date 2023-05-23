05f6bf41f29182253a4f06d49c9d605628546470
nagimasa_mbp <nagimasa.0114@gmail.com>
2023-05-23 11:59:27 +0900

    カテゴリ別の一覧ページでCSSと表示内容の修正

--- resources/views/user/category.blade.php
+++ resources/views/user/category.blade.php
@@ -1,105 +1,61 @@
 
 @include('layouts.user')
 
-<main class="category">
+<main class="category sm:max-w-3xl sm:m-auto">
 
 
 
     <article class="bg-white mb-5">
         <section class="px-2 py-4 border-gray-300 border-b border-t">
             <h2 class="text-2xl category-title">カテゴリ別</h2>
-            @foreach($services as $service)
-            <div class="item-pickup mb-10">
-                <a class="link-area" href="{{ route('user.detail', [$service->id])}}">
-                    <h3 class="text-xl p-4 text-white">{{ $service->service_name }}</h3>
-                    <div class="p-2">
-                        @if($service->phototop)
-                            <div class="img-box">
-                                <img src="{{ secure_asset($service->phototop->top_image_name) }}">
+            <div class="flex flex-wrap justify-start m-auto">
+                @foreach($services as $service)
+                <div class="item-pickup mb-10 mx-auto">
+                    <a class="link-area" href="{{ route('user.detail', [$service->id])}}">
+                        <h3 class="text-xl p-4 text-white">{{ $service->service_name }}</h3>
+                        <div class="p-2">
+                            @if($service->phototop)
+                                <div class="img-box">
+                                    <img src="{{ secure_asset($service->phototop->top_image_name) }}">
+                                </div>
+                            @endif
+                            <div class="py-2 border-b">
+                                @if($service->coupon)
+                                    <span class="badge-coupon">クーポン</span>
+                                @endif
+                                @if($service->reqruit)
+                                    <span class="badge-reqruit">求人</span>
+                                @endif
                             </div>
-                        @endif
-                        <div class="py-2 border-b">
-                            @if($service->coupon)
-                                <span class="badge-coupon">クーポン</span>
-                            @endif
-                            @if($service->reqruit)
-                                <span class="badge-reqruit">求人</span>
-                            @endif
+                            <dl class="item-data">
+                                <div class="flex py-2 border-b">
+                                    <dt class="font-bold w-32 text-right">地域：</dt>
+                                    <dd class="w-full">{{ $service->area->area_name}}</dd>
+                                </div>
+                                <div class="flex py-2 border-b">
+                                @if($service->comments)
+                                    <dt class="font-bold w-32 text-right">コメント：</dt>
+                                    <dd class="w-full">{{ $service->comments->comment}}</dd>
+                                @endif
+                                </div>
+                                <div class="flex py-2 border-b ">
+                                    <dt class="font-bold w-32 text-right">住所：</dt>
+                                    <dd class="w-full">{{ $service->address}}</dd>
+                                </div>
+                            </dl>
                         </div>
-                        <dl class="item-data">
-                            <div class="flex py-2 border-b">
-                                <dt class="font-bold w-32 text-right">地域：</dt>
-                                <dd class="w-full">{{ $service->area->area_name}}</dd>
-                            </div>
-                            <div class="flex py-2 border-b">
-                            @if($service->comments)
-                                <dt class="font-bold w-32 text-right">コメント：</dt>
-                                <dd class="w-full">{{ $service->comments->comment}}</dd>
-                            @endif
-                            </div>
-                            <div class="flex py-2 border-b ">
-                                <dt class="font-bold w-32 text-right">住所：</dt>
-                                <dd class="w-full">{{ $service->address}}</dd>
-                            </div>
-                        </dl>
-                    </div>
-                </a>
-                <dl class="item-data">
-                    <div class="flex pl-2 pb-2 border-b item-data">
-                        <dt class="font-bold w-32 text-right">問い合わせ：</dt>
-                        <dd class="w-full"><a href="tel:{{ $service->tel}}">{{ $service->tel }}</a></dd>
-                    </div>
-                </dl>
-            </div>
-            @endforeach
-        </section>
-    </article>
-
-
-
-    <article class="bg-white mb-5 blog">
-        <section class="px-2 py-4 border-gray-300 border-b border-t">
-            <h2 class="text-2xl blog-title">ブログの更新</h2>
-            <div class="blog-area p-4">
-                <div class="blog-item pb-2 border-b border-gray-200">
-                    {{-- ここに店舗カテゴリを判定してバッチを表示させる仕組みを入れる --}}
-                    <h3 class="text-base mb-2">店舗名</h3>
-                    <p class="text-xs mb-2">更新日時</p>
-                    <p class="text-xs font-bold mb-2">スタッフコメント</p>
+                    </a>
+                    <dl class="item-data">
+                        <div class="flex pl-2 pb-2 border-b item-data">
+                            <dt class="font-bold w-32 text-right">問い合わせ：</dt>
+                            <dd class="w-full"><a href="tel:{{ $service->tel}}">{{ $service->tel }}</a></dd>
+                        </div>
+                    </dl>
                 </div>
+                @endforeach
             </div>
         </section>
     </article>
-
-
-    <article class="bg-white mb-5 transpotation">
-        <section class="px-2 py-4 border-gray-300 border-b border-t">
-            <h2 class="text-2xl blog-title">交通</h2>
-            <div class="flex flex-wrap justify-around">
-                <a href="">
-                    <div class="category-item mb-1">
-                    </div>
-                </a>
-                <a href="">
-                    <div class="category-item mb-1">
-                    </div>
-                </a>
-            </div>
-        </section>
-    </article>
-
-
-
-
-    <article class="bg-white mb-5 reqruit">
-        <section class="px-2 py-4 border-gray-300 border-b border-t">
-            <h2 class="text-2xl reqruit-title">求人</h2>
-            <div class="">
-            </div>
-        </section>
-    </article>
-
-
 
     <article class="bg-white mb-5 typograph">
         <section class="px-2 py-4 border-gray-300 border-b border-t">
