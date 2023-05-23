4dfcc7eecdc89cc48d1e79d63576344a23499674
nagimasa_mbp <nagimasa.0114@gmail.com>
2023-05-23 14:55:42 +0900

    ギャラリーの非表示を修正

--- resources/views/user/detail.blade.php
+++ resources/views/user/detail.blade.php
@@ -419,4 +419,23 @@
 
 </main>
 
-@include('layouts.user-footer')
+<footer class="bg-white mb-5 transpotation">
+    <section class="px-2 py-4 border-gray-300 border-b border-t  sm:max-w-3xl m-auto">
+        <p class="publish-btn text-center text-white py-4 font-bold mb-4">掲載のお問い合わせ</p>
+        <ul>
+            <li class="text-xs pb-2"><a href="">当サイトについて</a></li>
+            <li class="text-xs pb-2"><a href="">掲載のお問い合わせ</a></li>
+            <li class="text-xs pb-2"><a href="">掲載規約</a></li>
+        </ul>
+    </section>
+    <section class="copyright py-7">
+        <p class="text-center text-white text-xs">copyright Gogo古賀</p>
+    </section>
+</footer>
+</body>
+
+
+
+<script src="{{ asset('js/menu.js') }}"></script>
+<script src="{{ asset('js/gallery.js') }}"></script>
+</html>
